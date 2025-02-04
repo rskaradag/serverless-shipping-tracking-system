4edb1b0d4c87a61d157ee84b3059a2ac27bf3a6a
nlskarad <selcuk.karadag@vanderlande.com>
2025-02-04 12:38:42 +0100

    added dynamodb and s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,7 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    bucket         = var.s3_bucket_name  
-    key            = "terraform/state.tfstate"  
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name  
-    encrypt        = true 
-  }
+  backend "s3" {}
 }
 
 
