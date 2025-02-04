a7d0091978424972c4dcd969ccbf3f9847f2c2d8
nlskarad <selcuk.karadag@vanderlande.com>
2025-02-04 15:44:05 +0100

    configured tf backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,33 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+data "aws_s3_bucket" "terraform_state" {
   bucket = "tfstate"
-     
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
-    bucket = aws_s3_bucket.terraform_state.id
-
-    versioning_configuration {
-      status = "Enabled"
-    }
+data "aws_dynamodb_table" "app-state" {
+  name = "app-state"
 }
 
-resource "aws_dynamodb_table" "app-state" {
-  name         = "app-state"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  lifecycle {
-    prevent_destroy = true
+terraform {
+  backend "s3" {
+    bucket         = data.aws_s3_bucket.terraform_state.bucket
+    key            = "terraform/state.tfstate"
+    region         = var.aws_region
+    dynamodb_table = data.aws_dynamodb_table.app-state.name
+    encrypt        = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,3 @@ variable "aws_region" {
   description = "AWS Deployment Region"
   type        = string
 }
-
-variable "s3_bucket_name" {
-  description = "S3 Bucket for Terraform State"
-   type        = string
-}
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB Table for Terraform State Lock"
-   type        = string
-}
