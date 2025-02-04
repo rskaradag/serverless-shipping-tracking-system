76f92f27c9b926d9a1f6022106d25c370aa1a0de
nlskarad <selcuk.karadag@vanderlande.com>
2025-02-04 12:43:13 +0100

    updated backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,30 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstate"
+     
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+resource "aws_s3_bucket_versioning" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.dynamodb_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "app-state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
   }
 }
-
-
