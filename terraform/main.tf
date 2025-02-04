a4f0f542127182fa653ad22bd1fcc01ce1cc7848
nlskarad <selcuk.karadag@vanderlande.com>
2025-02-04 12:00:33 +0100

    configured env variables on github

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = var.s3_bucket_name  # S3 Bucket adı
-    key            = "terraform/state.tfstate"  # State dosyası yolu
+    bucket         = var.s3_bucket_name  
+    key            = "terraform/state.tfstate"  
     region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name  # State lock için DynamoDB tablosu
-    encrypt        = true  # State dosyasını şifreleme
+    dynamodb_table = var.dynamodb_table_name  
+    encrypt        = true 
   }
 }
 
-# DynamoDB tablosu oluşturma (State Lock için)
+
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
