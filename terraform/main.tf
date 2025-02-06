dc6c91570aab7962e026632c0763bafd83bfcf30
rskaradag <r.selcukkaradag@gmail.com>
2025-02-06 13:50:18 +0100

    configured backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,20 +6,9 @@ data "aws_s3_bucket" "terraform_state" {
   bucket = "tf-serverless-shipping-tracking-system"
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
+data "aws_dynamodb_table" "terraform_lock" {
   name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform Lock Table"
-    Environment = "Production"
-  }
 }
 
 terraform {
