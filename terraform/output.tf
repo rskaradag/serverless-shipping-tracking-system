e0134a5418b82a891ebf2fda111f55716abd4e1d
rskaradag <r.selcukkaradag@gmail.com>
2025-02-06 14:17:24 +0100

    fixed output url

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,8 +3,8 @@ output "queue_URL" {
   value = aws_sqs_queue.order_queue.id
 }
 output "test_cURL" {
-  value = "curl -X POST -H 'Content-Type: application/json' -d '{\"username\":\"selcuk\", \"email\":\"r.selcukkaradag@gmail.com\", \"order_number\":\"6161\"}' ${aws_api_gateway_deployment.tracking_api_deployment.invoke_url}order"
+  value = "curl -X POST -H 'Content-Type: application/json' -d '{\"username\":\"selcuk\", \"email\":\"r.selcukkaradag@gmail.com\", \"order_number\":\"6161\"}' ${aws_api_gateway_stage.shipping_api.invoke_url}/order"
 }
 output "apigateway_url" {
-  value = aws_api_gateway_deployment.tracking_api_deployment.invoke_url
+  value = aws_api_gateway_stage.shipping_api.invoke_url
 }
