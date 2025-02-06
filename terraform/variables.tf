619d9e0e0eb7c882f1e76060d3cabd6fa91a2757
rskaradag <r.selcukkaradag@gmail.com>
2025-02-06 14:17:36 +0100

    added stage name as parameter

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,10 @@ variable "aws_region" {
   description = "AWS Deployment Region"
   type        = string
 }
+variable "stage_name" {
+  default = "dev"
+  type    = string
+}
 variable "dynamodb_table_name" {}
 variable "s3_bucket_name" {}
 variable "sqs_queue_name" {}
