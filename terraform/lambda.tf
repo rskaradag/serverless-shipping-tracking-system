81f91a2a1c6ced94beca1aec3c2e627ee1afceb2
rskaradag <r.selcukkaradag@gmail.com>
2025-02-06 13:50:30 +0100

    renamed lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,14 +2,22 @@ resource "aws_lambda_function" "create_tracking" {
   filename      = "create_tracking_lambda.zip"
   function_name = "createTrackingHandler"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
+  handler       = "create_tracking.handler"
   runtime       = "python3.9"
+  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
 
   environment {
     variables = {
       SQS_QUEUE_URL = aws_sqs_queue.order_queue.url
     }
   }
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.create_tracking.function_name
+  principal     = "apigateway.amazonaws.com"
 }
 
 # Create an IAM role for the Lambda function
@@ -59,4 +67,10 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
 resource "aws_iam_role_policy_attachment" "lambda_attach" {
   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
   role       = aws_iam_role.lambda_role.name
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_file = "../lambda/create_tracking/create_tracking.py"
+  output_path = "${path.module}/create_tracking_lambda.zip"
 }
